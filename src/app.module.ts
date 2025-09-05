1b70877754127a935e821ad189ccd7fef12b5d57
Carlos Vitor Cardoso da Silva <cvcardos5@gmail.com>
2025-09-05 12:02:38 -0300

    Adiciona rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,26 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { ScraperService } from './scraper/scraper.service';
-import { BotService } from './bot/bot.service';
-import { TelegrafModule } from 'nestjs-telegraf';
-import { FormatterService } from './formatter/formatter.service';
 import 'dotenv/config';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { Context } from 'telegraf';
+import rateLimit from 'telegraf-ratelimit';
+import { AppController } from './app.controller';
+import { BotService } from './bot/bot.service';
+import { FormatterService } from './formatter/formatter.service';
+import { ScraperService } from './scraper/scraper.service';
 
 @Module({
   imports: [
     TelegrafModule.forRoot({
       token: process.env.TELEGRAM_KEY!,
+      middlewares: [
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        rateLimit({
+          window: 10000,
+          limit: 3,
+          onLimitExceeded: (ctx: Context) =>
+            ctx.reply('Espere um pouco, por favor.'),
+        }),
+      ],
     }),
   ],
   controllers: [AppController],
